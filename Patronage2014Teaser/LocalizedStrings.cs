14914d9c5942a43dd0ff15430194536b1ef73dbf
Przemyslaw Nogaj <przemyslaw.nogaj@blstream.com>
2013-11-28 09:22:07 +0100

    Initial version of Windows Phone Patronage 2014 Teaser

---
+++ Patronage2014Teaser/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Patronage2014.Resources;
+
+namespace Patronage2014
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Patronage2014Teaser/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Patronage2014.Resources;
+
+namespace Patronage2014
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainPage_Loaded;
+            VisualStateManager.GoToState(this, "AllControlsVisible", true);
+        }
+
+    }
+}
